a3c3efaadc582bf6c7e927e214b28747ba7fc0a2
github username <github email address>
2019-01-09 16:15:06 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :name, :grade
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  
+end
+
+binding.pry
+true
