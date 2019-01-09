f1f4e2a8e2795bcceb9c44ee3ef3862bb06e3b71
github username <github email address>
2019-01-08 19:04:22 -0500

    school domain solution

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student,grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end
+
+  def grade(grade)
+    roster[grade]
+  end
+
+  def sort
+    sorted_List = {}
+    roster.each do |grade,students|
+      sorted_List[grade] = students.sort
+    end
+    sorted_List
+  end
+end
+
+school = School.new("Bayside High School")
