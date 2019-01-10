71c602b0ed8ab12f94e156841315280b0ef6bcda
Elijah Cleveland <elicleveland12@gmail.com>
2019-01-10 18:54:24 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+class School
+
+  attr_accessor :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+
+  def grade(grade)
+    new_array = []
+    roster.each do |my_grade, students|
+      if my_grade == grade
+        new_array << students
+      end
+    end
+    new_array.flatten
+  end
+
+  def sort
+    new_hash = {}
+    roster.each do |x, y|
+      new_hash[x] = y.sort
+    end
+    new_hash
+  end
+
+
+end
